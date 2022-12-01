fb6f331570fdb3eaaee33d8538210a446069f077
w32siooo <creeco@gmail.com>
2022-12-01 12:53:00 +0100

    try to fix runtime

--- day01/day1.groovy
+++ day01/day1.groovy
@@ -1,25 +1,32 @@
 import java.io.*
 println "groovy"
+
 File file = new File("input.txt")
 
 def list = [] // empty list
 def count = 0
+def largest = 0
 file.eachLine { line ->
    if(line==""){
     count++
-   }else {
-    if(list[count]==null){
+   } else {
+        if(list[count] == null){
             list.add(line.toInteger())
-    }else{
-        list[count] = list[count]+line.toInteger()
-    }
+        }else{
+          list[count] = list[count]+line.toInteger()
+         }
+        if(list[count] > largest){
+             largest = list[count]
+         }
    }
-        
 }
-list = list.sort()
+
+
 if(System.getenv("part").equals("part1")){
-    print list[count]
+    print largest
 }
 if(System.getenv("part").equals("part2")){
-    print "${list[count-2]+list[count-1]+list[count]}"
+    list.sort()
+    def top3 = list[-3]+list[-2]+list[-1]
+    print top3
 }
