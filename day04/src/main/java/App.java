10841ae69108f29731c3c7bfc8db5abcf55c1ba0
w32siooo <creeco@gmail.com>
2022-12-05 14:05:54 +0100

    day 4 optimizations

--- day04/src/main/java/App.java
+++ day04/src/main/java/App.java
@@ -2,11 +2,34 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
-
+import java.util.List;
 
 public class App {
-
     public static void main(final String[] args) throws IOException {
-        System.out.println(String.format("Java \n%s",System.getenv("part").equals("part1") ? "" + Files.lines(new File("input3.txt").toPath()).map(s -> s.split(",")).map(s -> Arrays.stream(s).map(range -> Arrays.stream(range.split("-")).map(Integer::parseInt).toList()).toList()).filter(s -> s.get(0).get(0) <= s.get(1).get(0) && s.get(0).get(1) >= s.get(1).get(1) || s.get(0).get(0) >= s.get(1).get(0) && s.get(0).get(1) <= s.get(1).get(1)).count() : "" + Files.lines(new File("input3.txt").toPath()).map(s -> s.split(",")).map(s -> Arrays.stream(s).map(range -> Arrays.stream(range.split("-")).map(Integer::parseInt).toList()).toList()).filter(s -> s.get(0).get(1) >= s.get(1).get(0) && s.get(0).get(1) <= s.get(1).get(1) || s.get(1).get(1) >= s.get(0).get(0) && s.get(1).get(1) <= s.get(0).get(1) || s.get(0).get(0) >= s.get(1).get(0) && s.get(0).get(0) <= s.get(1).get(1) || s.get(1).get(0) >= s.get(0).get(0) && s.get(1).get(0) <= s.get(0).get(1)).count()));
+        System.out.println(String.format("Java \n%s", System.getenv("part").equals("part1")
+                ? Files.lines(new File("input3.txt").toPath())
+                .map(s -> s.split(","))
+                .map(s -> List.of(
+                        Arrays.stream(Arrays.asList(s).get(0).split("-"))
+                                .mapToInt(Integer::parseInt)
+                                .toArray(),
+                        Arrays.stream(Arrays.asList(s).get(1).split("-"))
+                                .mapToInt(Integer::parseInt)
+                                .toArray())
+                )
+                .filter(s -> s.get(0)[0] <= s.get(1)[0] && s.get(0)[1] >= s.get(1)[1] || s.get(0)[0] >= s.get(1)[0] && s.get(0)[1] <= s.get(1)[1])
+                .count()
+                : Files.lines(new File("input3.txt")
+                        .toPath())
+                .map(s -> s.split(","))
+                .map(s -> List.of(
+                        Arrays.stream(Arrays.asList(s).get(0).split("-"))
+                                .mapToInt(Integer::parseInt)
+                                .toArray(),
+                        Arrays.stream(Arrays.asList(s).get(1).split("-"))
+                                .mapToInt(Integer::parseInt)
+                                .toArray()))
+                .filter(s -> s.get(0)[1] >= s.get(1)[0] && s.get(0)[1] <= s.get(1)[1] || s.get(1)[1] >= s.get(0)[0] && s.get(1)[1] <= s.get(0)[1] || s.get(0)[0] >= s.get(1)[0] && s.get(0)[0] <= s.get(1)[1] || s.get(1)[0] >= s.get(0)[0] && s.get(1)[0] <= s.get(0)[1])
+                .count()));
     }
 }
