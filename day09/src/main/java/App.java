1d447ecd75fe13cc39fe28061faab02ba590a869
w32siooo <creeco@gmail.com>
2022-12-09 14:31:28 +0100

    fewer lines

--- day09/src/main/java/App.java
+++ day09/src/main/java/App.java
@@ -2,11 +2,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class App {
-    private static final List<int[]> rope = new ArrayList<>();
-    private static int ropeLength = 0;
+    private static List<int[]> rope;
+
     public static void main(String[] args) throws IOException {
 
         System.out.println("Java");
@@ -21,7 +22,7 @@ public class App {
 
         var set = new HashSet<String>();
 
-        for (int i = 0; i < Integer.parseInt(input[1]); i++) {
+        for (int i = 0; i < Integer.parseInt(input[1]); i++) { //Move the rope x times.
             switch (input[0]) {
                 case "R" -> rope.get(0)[1] = rope.get(0)[1] + 1;
                 case "U" -> rope.get(0)[0] = rope.get(0)[0] - 1;
@@ -29,42 +30,39 @@ public class App {
                 case "D" -> rope.get(0)[0] = rope.get(0)[0] + 1;
             }
 
-            for (int knotIndex = 1; knotIndex < rope.size(); knotIndex++) {
-                int dist = (int) Math.sqrt((rope.get(knotIndex - 1)[0] - rope.get(knotIndex)[0])
-                        * (rope.get(knotIndex - 1)[0] - rope.get(knotIndex)[0])
-                        + (rope.get(knotIndex - 1)[1] - rope.get(knotIndex)[1])
-                        * (rope.get(knotIndex - 1)[1] - rope.get(knotIndex)[1]));
+            for (int knot = 1; knot < rope.size(); knot++) {
+                double dist = (int) Math.sqrt(
+                        (rope.get(knot - 1)[0] - rope.get(knot)[0])
+                                * (rope.get(knot - 1)[0] - rope.get(knot)[0])
+                                + (rope.get(knot - 1)[1] - rope.get(knot)[1])
+                                * (rope.get(knot - 1)[1] - rope.get(knot)[1]));
 
                 if (dist > 1) {
-                    if (rope.get(knotIndex - 1)[1] - rope.get(knotIndex)[1] >= 1) {
-                        rope.get(knotIndex)[1]++;
+                    if (rope.get(knot - 1)[1] - rope.get(knot)[1] >= 1) {
+                        rope.get(knot)[1]++;
                     }
-                    if (rope.get(knotIndex - 1)[1] - rope.get(knotIndex)[1] <= -1) {
-                        rope.get(knotIndex)[1]--;
+                    if (rope.get(knot - 1)[1] - rope.get(knot)[1] <= -1) {
+                        rope.get(knot)[1]--;
                     }
 
-                    if (rope.get(knotIndex - 1)[0] - rope.get(knotIndex)[0] >= 1) {
-                        rope.get(knotIndex)[0]++;
+                    if (rope.get(knot - 1)[0] - rope.get(knot)[0] >= 1) {
+                        rope.get(knot)[0]++;
                     }
-                    if (rope.get(knotIndex - 1)[0] - rope.get(knotIndex)[0] <= -1) {
-                        rope.get(knotIndex)[0]--;
+                    if (rope.get(knot - 1)[0] - rope.get(knot)[0] <= -1) {
+                        rope.get(knot)[0]--;
                     }
                 }
 
             }
-            set.add(Arrays.toString(rope.get(ropeLength - 1)));
-
+            set.add(Arrays.toString(rope.get(rope.size() - 1)));
         }
         return set;
-
     }
 
     private static long solve(int kCount) throws IOException {
-        ropeLength = kCount;
-        for (int i = 0; i < kCount; i++) {
-            rope.add(new int[]{0, 0});
-        }
-
+        rope = IntStream.range(0, kCount)
+                .mapToObj(s -> new int[]{0, 0})
+                .toList();
         try (Stream<String> lines = Files.lines(new File("input.txt").toPath())) {
             return lines.map(s -> s.split(" "))
                     .map(App::solve)
